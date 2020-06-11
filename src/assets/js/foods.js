62323f0b9a071db443bf77bafd991a2a9bb15a50
Marcos Oliveira <marcosoliveirasamcro@gmail.com>
2020-06-11 19:29:59 -0300

    menu page finishd

---
+++ src/assets/js/foods.js
@@ -0,0 +1,32 @@
+export const foods = [
+  {
+    Name: "Pellentesque",
+    Price: 11.99,
+    Ingredients: ["metus", "elementum", "mattis", "maximus"],
+  },
+  {
+    Name: "Curabitur",
+    Price: 10.99,
+    Ingredients: ["dignissim", "Nullam", "metus", "volutpat"],
+  },
+  {
+    Name: "Pellentesque",
+    Price: 16.99,
+    Ingredients: ["metus", "elementum", "mattis", "maximus"],
+  },
+  {
+    Name: "Curabitur ",
+    Price: 22.99,
+    Ingredients: ["dignissim", "Nullam", "metus", "volutpat"],
+  },
+  {
+    Name: "Pellentesque",
+    Price: 16.99,
+    Ingredients: ["metus", "elementum", "mattis", "maximus"],
+  },
+  {
+    Name: "Curabitur ",
+    Price: 22.99,
+    Ingredients: ["dignissim", "Nullam", "metus", "volutpat"],
+  },
+];
--- src/menu.js
+++ src/menu.js
@@ -1,16 +1,46 @@
 import { load } from "./changePages";
+import { foods } from "./assets/js/foods";
 
-// const miniBanner = () => {
-//   const section = document.createElement("section");
-//   section.classList.add("miniBanner");
-//   section.innerHTML = "Duis congue lectus vitae porta vulputate";
-//   return section
-// };
+const foodSection = () => {
+  const section = document.createElement("section");
+  const divFoods = document.createElement("div");
+
+  section.classList.add("foodSection");
+  divFoods.classList.add("foods");
+
+  foods.forEach((food) => {
+    const divFood = document.createElement("div");
+    const name = document.createElement("h4");
+    const price = document.createElement("span");
+    const list = document.createElement("ul");
+
+    divFood.classList.add('food');
+    name.innerHTML = food.Name;
+    price.innerHTML = food.Price;
+
+    food.Ingredients.forEach((ingrendient) => {
+      const itemList = document.createElement("li");
+      itemList.innerHTML = ingrendient;
+
+      list.appendChild(itemList);
+    });
+
+    divFood.appendChild(name);
+    divFood.appendChild(price);
+    divFood.appendChild(list);
+
+    divFoods.appendChild(divFood);
+  });
+
+  section.appendChild(divFoods);
+  
+  return section;
+};
 
 const loadMenu = () => {
   load();
-  // const content = document.querySelector("#content");
-  // content.appendChild(miniBanner());
+  const content = document.querySelector('#content');
+  content.appendChild(foodSection());
 };
 
 export { loadMenu };
